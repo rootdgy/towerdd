e1fce46f2df92b90e38735e1bcf0fed9cd3612d1
rootdgy <47914644+rootdgy@users.noreply.github.com>
2025-11-29 21:36:31 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/towerdd/', // !!! 必须与你的 GitHub 仓库名一致，前后都要加斜杠 !!!
+  base: '/towerdd/', // Matches the repo name found in your error log
 })
